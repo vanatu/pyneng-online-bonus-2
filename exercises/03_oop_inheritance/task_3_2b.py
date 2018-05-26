e8e68831ff05e255c8d2952dc1d9b9adf66a2ade
pyneng <pyneng.course@gmail.com>
2018-05-26 11:02:47 +0000

    Задания 03_oop_inheritance

---
+++ exercises/03_oop_inheritance/task_3_2b.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+'''
+Задание 3.2b
+
+Дополнить класс MyNetmiko из задания 3.2a.
+
+Переписать метод send_config_set netmiko, добавив в него проверку на ошибки с помощью метода _check_error_in_command.
+
+Метод send_config_set должен отправлять команды по одной и проверять каждую на ошибки.
+Если при выполнении команд не обнаружены ошибки, метод send_config_set возвращает вывод команд.
+
+In [2]: from task_3_2b import MyNetmiko
+
+In [3]: r1 = MyNetmiko(**device_params)
+
+In [4]: r1.send_config_set('lo')
+---------------------------------------------------------------------------
+ErrorInCommand                            Traceback (most recent call last)
+<ipython-input-2-8e491f78b235> in <module>()
+----> 1 r1.send_config_set('lo')
+
+...
+ErrorInCommand: При выполнении команды "lo" на устройстве 192.168.100.1 возникла ошибка "Incomplete command."
+
+'''
+
+from netmiko.cisco.cisco_ios import CiscoIosBase
+
+
+device_params = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.100.1',
+    'username': 'cisco',
+    'password': 'cisco',
+    'secret': 'cisco'
+}
+
+
+
---
+++ exercises/03_oop_inheritance/task_3_2d.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+'''
+Задание 3.2d
+
+Дополнить класс MyNetmiko из задания 3.2c или задания 3.2b.
+
+Добавить параметр ignore_errors в метод send_config_set.
+Если передано истинное значение, не надо выполнять проверку на ошибки и метод должен работать точно так же как метод send_config_set в netmiko.
+
+Если значение ложное, ошибки должны проверяться.
+
+По умолчанию ошибки должны игнорироваться.
+
+
+In [2]: from task_3_2d import MyNetmiko
+
+In [3]: r1 = MyNetmiko(**device_params)
+
+In [6]: r1.send_config_set('lo')
+Out[6]: 'config term\nEnter configuration commands, one per line.  End with CNTL/Z.\nR1(config)#lo\n% Incomplete command.\n\nR1(config)#end\nR1#'
+
+In [7]: r1.send_config_set('lo', ignore_errors=True)
+Out[7]: 'config term\nEnter configuration commands, one per line.  End with CNTL/Z.\nR1(config)#lo\n% Incomplete command.\n\nR1(config)#end\nR1#'
+
+In [8]: r1.send_config_set('lo', ignore_errors=False)
+---------------------------------------------------------------------------
+ErrorInCommand                            Traceback (most recent call last)
+<ipython-input-8-704f2e8d1886> in <module>()
+----> 1 r1.send_config_set('lo', ignore_errors=False)
+
+...
+ErrorInCommand: При выполнении команды "lo" на устройстве 192.168.100.1 возникла ошибка "Incomplete command."
+'''
+
+from netmiko.cisco.cisco_ios import CiscoIosBase
+
+
+device_params = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.100.1',
+    'username': 'cisco',
+    'password': 'cisco',
+    'secret': 'cisco'
+}
+
+
+
